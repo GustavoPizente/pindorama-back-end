f7751dae37e1ea718bbc04318036d5d86a93a9cb
GustavoPizente <gustavonazarine@gmail.com>
2024-06-23 19:40:57 -0300

    deployVercel

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ExpressAdapter } from '@nestjs/platform-express';
-import * as express from 'express';
-import serverlessExpress from '@vendia/serverless-express';
-
-const expressApp = express();
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, new ExpressAdapter(expressApp));
+  const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: true, // Permite requisições de qualquer origem
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    allowedHeaders: 'Content-Type, Authorization',
+  });
   await app.listen(5000);
 }
-
 bootstrap();
